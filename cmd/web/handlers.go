88c030cf059de4d31f32a5f51cb96dced6eeb6bc
SGRashid <s.gabdullin@gmail.com>
2021-10-27 21:45:37 +0500

    Добавил файлы

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -4,6 +4,8 @@ import (
     "fmt"
     "net/http"
     "strconv"
+	"html/template"
+	"log"
 )
  
 func home(w http.ResponseWriter, r *http.Request) {
@@ -12,7 +14,18 @@ func home(w http.ResponseWriter, r *http.Request) {
         return
     }
  
-    w.Write([]byte("Привет из Snippetbox"))
+    ts, err := template.ParseFiles("./ui/html/home.page.tmpl")
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.Execute(w, nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
 }
  
 func showSnippet(w http.ResponseWriter, r *http.Request) {
