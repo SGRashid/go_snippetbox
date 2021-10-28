cfb62e4f391c1b31c8af56223b410f335eca85ed
SGRashid <s.gabdullin@gmail.com>
2021-10-28 20:42:18 +0500

    Ограничиваем доступ к папке static

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,8 @@ package main
 import (
     "log"
     "net/http"
+    "path/filepath"
 )
-
-const port = ":4000"
  
 func main() {
     mux := http.NewServeMux()
@@ -13,11 +12,39 @@ func main() {
     mux.HandleFunc("/snippet", showSnippet)
     mux.HandleFunc("/snippet/create", createSnippet)
  
-    fileServer := http.FileServer(http.Dir("./ui/static"))
+    fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
 
     mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
     log.Printf("Сервер запущен на localhost%v", port)
     err := http.ListenAndServe(port, mux)
     log.Fatal(err)
+}
+
+const port = ":4000"
+
+type neuteredFileSystem struct {
+    fs http.FileSystem
+}
+
+func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
+    f, err := nfs.fs.Open(path)
+    if err != nil {
+        return nil, err
+    }
+
+    s, err := f.Stat()
+    if s.IsDir() {
+        index := filepath.Join(path, "index.html")
+        if _, err := nfs.fs.Open(index); err != nil {
+            closeErr := f.Close()
+            if closeErr != nil {
+                return nil, closeErr
+            }
+
+            return nil, err
+        }
+    }
+
+    return f, nil
 }
