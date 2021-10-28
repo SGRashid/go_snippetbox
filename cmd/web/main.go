cd7c8562f2589a19d4170ae75d935ec39e12d5dc
SGRashid <s.gabdullin@gmail.com>
2021-10-28 20:14:21 +0500

    Отображаем стили , картинки и используем JS

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,10 @@ func main() {
     mux.HandleFunc("/snippet", showSnippet)
     mux.HandleFunc("/snippet/create", createSnippet)
  
+    fileServer := http.FileServer(http.Dir("./ui/static"))
+
+    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
     log.Printf("Сервер запущен на localhost%v", port)
     err := http.ListenAndServe(port, mux)
     log.Fatal(err)
