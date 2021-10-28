3b2bbae6f0014abf4ce70600f3c6881880bece9b
Sergey Gabdullin <s.gabdullin@gmail.com>
2021-10-28 23:51:44 +0500

    Добавил флаг для адреса

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,50 +1,54 @@
 package main
- 
+
 import (
-    "log"
-    "net/http"
-    "path/filepath"
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
 )
- 
+
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
-    mux.HandleFunc("/snippet", showSnippet)
-    mux.HandleFunc("/snippet/create", createSnippet)
- 
-    fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+	addr := flag.String("addr", defaultAdress, "Сетевой адрес HTTP")
+	flag.Parse()
 
-    mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-    log.Printf("Сервер запущен на localhost%v", port)
-    err := http.ListenAndServe(port, mux)
-    log.Fatal(err)
+	fileServer := http.FileServer(neuteredFileSystem{http.Dir("./static")})
+
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	log.Printf("Сервер запущен на localhost%v", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	log.Fatal(err)
 }
 
-const port = ":4000"
+const defaultAdress = ":4000"
 
 type neuteredFileSystem struct {
-    fs http.FileSystem
+	fs http.FileSystem
 }
 
 func (nfs neuteredFileSystem) Open(path string) (http.File, error) {
-    f, err := nfs.fs.Open(path)
-    if err != nil {
-        return nil, err
-    }
+	f, err := nfs.fs.Open(path)
+	if err != nil {
+		return nil, err
+	}
 
-    s, err := f.Stat()
-    if s.IsDir() {
-        index := filepath.Join(path, "index.html")
-        if _, err := nfs.fs.Open(index); err != nil {
-            closeErr := f.Close()
-            if closeErr != nil {
-                return nil, closeErr
-            }
+	s, err := f.Stat()
+	if s.IsDir() {
+		index := filepath.Join(path, "index.html")
+		if _, err := nfs.fs.Open(index); err != nil {
+			closeErr := f.Close()
+			if closeErr != nil {
+				return nil, closeErr
+			}
 
-            return nil, err
-        }
-    }
+			return nil, err
+		}
+	}
 
-    return f, nil
+	return f, nil
 }
