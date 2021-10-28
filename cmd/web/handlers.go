4fc4d7d02233c97e8e503a31b8ef4e60691f20bb
Sergey Gabdullin <s.gabdullin@gmail.com>
2021-10-29 01:06:24 +0500

    исправления

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -1,26 +1,26 @@
 package main
- 
+
 import (
-    "fmt"
-    "net/http"
-    "strconv"
+	"fmt"
 	"html/template"
 	"log"
+	"net/http"
+	"strconv"
 )
- 
-func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
 
-    files := []string{
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
-        "./ui/html/footer.partial.tmpl",
-    }
- 
-    ts, err := template.ParseFiles(files...)
+		"./ui/html/footer.partial.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Println(err.Error())
 		http.Error(w, "Internal Server Error", 500)
@@ -33,23 +33,24 @@ func home(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Internal Server Error", 500)
 	}
 }
- 
+
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-    id, err := strconv.Atoi(r.URL.Query().Get("id"))
-    if err != nil || id < 1 {
-        http.NotFound(w, r)
-        return
-    }
- 
-    fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Отображение определенной заметки с ID %d...", id)
 }
- 
+
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        http.Error(w, "Метод не дозволен", 405)
-        return
-    }
- 
-    w.Write([]byte("Создание новой заметки..."))
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		const err405 = 405
+		http.Error(w, "Метод не дозволен", err405)
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
 }
